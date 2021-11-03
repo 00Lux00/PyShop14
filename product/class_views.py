d1b0d91de0841ecb8021153f53519243a60fa14d
00Lux00 <esenbolot1203@gmail.com>
2021-11-03 16:15:36 +0600

    CRUD, CBV

---
+++ product/class_views.py
@@ -0,0 +1,27 @@
+from django.views.generic import ListView, DetailView
+
+from product.models import Category, Product
+
+
+class CategoryListView(ListView):
+    model = Category  # Category.objects.all()
+    template_name = 'home.html'
+    context_object_name = 'categories'
+
+
+class ProductListView(ListView):
+    model = Product  # Product.objects.all()
+    template_name = 'product_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        print(self.kwargs)
+        # queryset = queryset.filter(category__slug=)
+        return queryset
+
+class ProductDetailView(DetailView):
+    model = Product #Product.objects.get(product_id)
+    template_name = 'detail.html'
+    context_object_name = 'product'
+    pk_url_kwarg = 'product_id'
